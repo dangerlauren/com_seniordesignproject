d190118605194eddf7c33330065605d2e28f6147
Grant <grantlauren@wireless-10-145-232-185.public.utexas.edu>
2016-03-31 14:49:47 -0500

    added basic backend

---
+++ admin/controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_seniordesignproject
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * General Controller of SeniorDesignProject component
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_seniordesignproject
+ * @since       0.0.7
+ */
+class SeniorDesignProjectController extends JControllerLegacy
+{
+	/**
+	 * The default view for the display method.
+	 *
+	 * @var string
+	 * @since 12.2
+	 */
+	protected $default_view = 'seniordesignprojects';
+}
--- admin/seniordesignproject.php
+++ admin/seniordesignproject.php
@@ -1,1 +1,20 @@
-Senior Design Project admin
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_seniordesignproject
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// Get an instance of the controller prefixed by SeniorDesignProject
+$controller = JControllerLegacy::getInstance('SeniorDesignProject');
+ 
+// Perform the Request task
+$controller->execute(JFactory::getApplication()->input->get('task'));
+ 
+// Redirect if set by the controller
+$controller->redirect();
