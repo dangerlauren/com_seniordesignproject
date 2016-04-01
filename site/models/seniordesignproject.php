2b56294062c1ae132c15ff0dfeb0c80e3707e0f7
Grant <grantlauren@wireless-10-145-74-128.public.utexas.edu>
2016-04-01 11:07:18 -0500

    reverting changes made April 1

--- site/models/seniordesignproject.php
+++ site/models/seniordesignproject.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class SeniorDesignProjectModelDefault extends JModelItem
+class SeniorDesignProjectModelSeniorDesignProject extends JModelItem
 {
 	protected function getListQuery()
 	{
--- site/views/seniordesignproject/view.html.php
+++ site/views/seniordesignproject/view.html.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die('Restricted access');
  *
  * @since  0.0.1
  */
-class SeniorDesignProjectViewDefault extends JViewLegacy
+class SeniorDesignProjectViewSeniorDesignProject extends JViewLegacy
 {
 	/**
 	 * Display the Senior Design Project view
